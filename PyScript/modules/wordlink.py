277ebf6edda3e0a6da3a36b014933a2fbeb6312b
ianmcd34 <106119861+ianmcd34@users.noreply.github.com>
2022-10-15 15:22:56 +0100

    Update wordlink.py

--- PyScript/modules/wordlink.py
+++ PyScript/modules/wordlink.py
@@ -80,6 +80,7 @@ while True:
         diffs.append(SequenceMatcher(list1[0], list1[-1]))
         
     if min(diffs)==0:
+        list1=links[diffs.index(min(diffs))]
         break
-list1=links[diffs.index(min(diffs))]
+
 print(list1)
