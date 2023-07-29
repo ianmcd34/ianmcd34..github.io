8ed3ce4f5b1903c22b14b32cb4ea8cdda35deb85
eipi45 <106119861+ianmcd34@users.noreply.github.com>
2023-07-29 13:07:45 +0100

    Update class_Expression.py

--- PyScript/modules/class_Expression.py
+++ PyScript/modules/class_Expression.py
@@ -164,18 +164,17 @@ class Expression:
             b=re.search(searchtxt, result)
             print(b)
             print("depths="+str(self.find_depth(firstrednode))+","+str(self.find_depth(lastrednode)))
+            result1=result
             if self.find_depth(firstrednode)>self.find_depth(lastrednode):
                 print("depth1>depth2")
                 a=result[0:a].rindex('!(')
                 result1=result[0:a]+"R"+result[a:b.span()[1]]+"S"+result[b.span()[1]:]
             if self.find_depth(firstrednode)<self.find_depth(lastrednode):
-                print("depth1<depth2",result[0:a]+"R"+result[a:b.span()[1]]+result[b.span()[1]:b.span()[1]+result[b.span()[1]:].index('!)')+2],result[b.span()[1]+result[b.span()[1]:].index('!)')+2:])
-                
+                print("depth1<depth2",result[0:a]+"R"+result[a:b.span()[1]]+result[b.span()[1]:b.span()[1]+result[b.span()[1]:].index('!)')+2],result[b.span()[1]+result[b.span()[1]:].index('!)')+2:])           
                 result1=result[0:a]+"R"+result[a:b.span()[1]]+result[b.span()[1]:b.span()[1]+result[b.span()[1]:].index('!)')+2]+"S"+result[b.span()[1]+result[b.span()[1]:].index('!)')+2:]
-            print(result, result1)
+
             result=result1
-            print(result, result1)
-   
+
         output=result.replace("(!", "{")
         output=output.replace("!)","}")
         output=output.replace("(","")
