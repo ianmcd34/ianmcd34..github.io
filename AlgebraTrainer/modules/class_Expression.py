27796294c94fdf96bbecc8a7a7d4e4e3048d2a88
eipi45 <106119861+ianmcd34@users.noreply.github.com>
2023-09-24 13:14:25 +0100

    Update class_Expression.py

--- AlgebraTrainer/modules/class_Expression.py
+++ AlgebraTrainer/modules/class_Expression.py
@@ -898,12 +898,12 @@ class Expression:
             return 999
         
         for e in self.elements:
-            print("multiply consts  e="+e)
+            print("multiply consts  e="+str(e))
             commonparentindex=None
             if e[1][-1].isdigit():
                 print("multiply consts digit found")
                 for e1 in self.elements:
-                    print("multiply consts  e1="+e1)
+                    print("multiply consts  e1="+str(e1))
                     constantindex=None
                     constantindex1=None
                     commonparentindex=None
